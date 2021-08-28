b5af546ee66dbb1d7176b30915be4da10ff6da1e
jake <jakedlee2018@gmail.com>
2021-08-28 04:55:52 -0500

    post feature done

--- models/post.js
+++ models/post.js
@@ -39,4 +39,4 @@ Post.init(
     }
   );
 
-  module.exports = post;
+  module.exports = Post;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+// const mysql = require('mysql2');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,10 +9,26 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// found solution at https://stackoverflow.com/questions/40477625/nodejs-mysql-er-access-denied-error-access-denied-for-user-rootlocalhost
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Yakmeister12'
+// });
+
+// connection.query(
+//     function(err, results,) {
+//         console.log(err);
+//         console.log(results);
+//         connection.end();
+//     }
+// );
+
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+ 
